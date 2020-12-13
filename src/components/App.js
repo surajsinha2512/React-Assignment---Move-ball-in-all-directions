2aeaea06398754f843a1ad08c378f28654ba2b9f
surajsinha2512 <61259615+surajsinha2512@users.noreply.github.com>
2020-12-13 19:37:49 +0530

    final

--- src/components/App.js
+++ src/components/App.js
@@ -10,51 +10,53 @@ const App = () => {
     top: "0px",
   });
   const reset = () => {
-    setX(0)
-    setY(0);
-    setBallPosition({
-      left:"0px",
-      top:"0px"
-    })
-     setRenderBall(false)
-    
+    setRenderBall(false)
+    updateXY(0,0)
+      
   };
   const renderChoice = () => {
     return renderBall?<div className="ball" style={{position:'absolute',
     left:ballPosition.left,
     top:ballPosition.top
-    }}></div>:<button onClick={start} className="start">start</button>
+    }}></div>:(<button onClick={start} className="start">start</button>)
   };
 
  const start=()=>{
-     setRenderBall(true)
+     setRenderBall(true);
 
  }
 
+const  updateXY=(newX,newY)=>{
+setX(newX)
+setY(newY)
+setBallPosition({
+  left:newX+'px',
+  top:newY+'px'
+})
+}
 
 useEffect(()=>{
-
   const eventList=(evt)=>{
     if (renderBall){
     if(evt.keyCode===37){
-     setX(x-5)
+    // setX(x-5)
+     updateXY(x-5,y)
     }
    else if(evt.keyCode===38){
-      setY(y-5)
+     // setY(y-5)
+      updateXY(x,y-5)
     }
     else if(evt.keyCode===39){
-      setX(x+5)
+    //  setX(x+5)
+      updateXY(x+5,y)
     }
     else if(evt.keyCode===40){
-      setY(y+5)
+    //  setY(y+5)
+      updateXY(x,y+5)
     }
   }
-    console.log("key pressed"+renderBall+" "+x+" "+y)
-    setBallPosition({
-      left: x+"px",
-      top: y+"px"
-    
-    })
+    console.log("key pressed "+renderBall+" "+x+" "+y)
+  
   }
   document.addEventListener("keydown",eventList)
   return ()=>document.removeEventListener("keydown",eventList)
